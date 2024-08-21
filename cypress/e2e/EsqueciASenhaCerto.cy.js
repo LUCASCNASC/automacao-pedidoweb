ee375f176ba9b09a444a5ff6decbb64609c25c0e
Lucas Camarmo do Nascimento <nascimentocamargolucas@gmail.com>
2024-08-21 15:09:47 -0300

    commit projeto

--- cypress/e2e/EsqueciASenhaCerto.cy.js
+++ cypress/e2e/EsqueciASenhaCerto.cy.js
@@ -1,18 +1,11 @@
 describe('Esqueci a senha', () => {
 
     beforeEach(() => {
-      cy.login()
+      
     })
   
     it('Colocar senha errada e tentar fazer a recuperação da mesma.', () => {
-        cy.get('.btn > .text-dark-100').click();
-        cy.get('.navi-footer > .btn').click();
-        cy.get('#email').type('TESTE {downArrow}');
-        cy.get('#current-password').type('TESTE {downArrow}');
-        cy.get('#kt_login_signin_submit').click();
-        cy.get('#kt_login_forgot').click();
-        cy.get('.form-control').type('lucascamargo.ncs@gmail.com {downArrow}');
-        cy.get('.btn-primary').click();
-        //ainda não está pronto
+        cy.visit('/principal')
+
     })
 })
