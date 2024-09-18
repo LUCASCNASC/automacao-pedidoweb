99d66623ac8c8dcd9967535acfe7738b4ece9fd1
Elizaine <elizaine.orosco@gmail.com>
2024-09-18 15:10:04 -0300

    teste

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   e2e: {
    baseUrl: 'http://10.7.0.42:2800/',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      //; implement node event listeners here
     },
   },
 });
